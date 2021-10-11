f8d6c224009eb4a5ee51355f40e83c522ade86c6
albnnc <albnnc@gmail.com>
2021-10-11 13:47:51 +0300

    feat(restyler-theme-delta): bump deps, add modal heading styles

--- packages/restyler-theme-delta/src/theme/heading.ts
+++ packages/restyler-theme-delta/src/theme/heading.ts
@@ -8,35 +8,49 @@ export const heading: BasicTheme = {
     letterSpacing: '0.04em',
     fontWeight: 'heading',
     lineHeight: 'heading',
-    color: 'inherit'
+    color: 'inherit',
+    '&:is(h1)': { mt: 3, mb: 2, fontSize: 8 },
+    '&:is(h2)': { mt: 3, mb: 2, fontSize: 7 },
+    '&:is(h3)': { mt: 3, mb: 2, fontSize: 6 },
+    '&:is(h4)': { mt: 2, mb: 1, fontSize: 5 },
+    '&:is(h5)': { mt: 2, mb: 1, fontSize: 4 },
+    '&:is(h6)': { mt: 2, mb: 1, fontSize: 3 }
   },
   kinds: {
-    1: { style: { mt: 3, mb: 2, fontSize: 8 } },
-    2: { style: { mt: 3, mb: 2, fontSize: 7 } },
-    3: { style: { mt: 3, mb: 2, fontSize: 6 } },
-    4: { style: { mt: 2, mb: 1, fontSize: 5 } },
-    5: { style: { mt: 2, mb: 1, fontSize: 4 } },
-    6: { style: { mt: 2, mb: 1, fontSize: 3 } },
+    modal: {
+      style: {
+        '&&': {
+          textTransform: 'uppercase',
+          color: 'inherit',
+          fontSize: 4,
+          letterSpacing: '0.04em'
+        }
+      }
+    },
     layout: {
       style: {
-        margin: 0,
-        textTransform: 'uppercase',
-        color: 'inherit',
-        fontSize: 6,
-        letterSpacing: '0.04em',
-        // Always on dark.
-        fontWeight: 300
+        '&&': {
+          margin: 0,
+          textTransform: 'uppercase',
+          color: 'inherit',
+          fontSize: 6,
+          letterSpacing: '0.04em',
+          // Always on dark.
+          fontWeight: 300
+        }
       }
     },
     feedItem: {
       style: {
-        marginY: 0,
-        marginX: 0,
-        textTransform: 'uppercase',
-        color: 'inherit',
-        fontSize: 4,
-        fontWeight: 'heading',
-        letterSpacing: '0.04em'
+        '&&': {
+          marginY: 0,
+          marginX: 0,
+          textTransform: 'uppercase',
+          color: 'inherit',
+          fontSize: 4,
+          fontWeight: 'heading',
+          letterSpacing: '0.04em'
+        }
       }
     }
   }
