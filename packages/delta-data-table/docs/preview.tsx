ff0536857b2ef7de0f5d16fbc9fc2929e3ebb292
albnnc <albnnc@gmail.com>
2021-12-08 16:43:36 +0300

    refactor(delta-data-table): move deps, rework docs

--- packages/delta-data-table/docs/preview.tsx
+++ packages/delta-data-table/docs/preview.tsx
@@ -2,7 +2,8 @@ import { jsx } from '@theme-ui/core';
 import { AppContainer } from 'delta-layout';
 
 export const parameters = {
-  controls: { hideNoControlsWarning: true }
+  controls: { hideNoControlsWarning: true },
+  layout: 'fullscreen'
 };
 
 export const decorators = [
@@ -14,25 +15,3 @@ export const decorators = [
     );
   }
 ];
-
-const commonNamespace = {
-  sections: {
-    feedItemRegistry: 'Item Registry',
-    feedSectionColumns: 'Section Columns'
-  },
-  actions: {
-    apply: 'Apply',
-    cancel: 'Cancel'
-  },
-  labels: {
-    anyCountThatFit: 'Any Count That Fit',
-    count: 'Count',
-    fixedCount: 'Fixed Count',
-    layoutType: 'Layout Type',
-    minimumWidth: 'Minimum Width',
-    search: 'Search'
-  },
-  errors: {
-    wrongFormat: 'Wrong format'
-  }
-};
