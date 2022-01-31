ce93297b5a5081bbe7f80d6d82d84c0695a7e5d7
albnnc <albnnc@gmail.com>
2022-01-31 11:30:17 +0300

    fix: tooltip container

--- packages/delta-tooltip/lib/Tooltip.tsx
+++ packages/delta-tooltip/lib/Tooltip.tsx
@@ -1,6 +1,7 @@
 import { jsx } from '@theme-ui/core';
 import Tippy, { TippyProps } from '@tippyjs/react';
-import { useThemed } from 'restyler';
+import { useContext } from 'react';
+import { SystemContext, useIsomorphicLayoutEffect, useThemed } from 'restyler';
 
 // TODO: Remove and use theme styles. When CSS files
 // will be removed from target build, one should
@@ -8,13 +9,30 @@ import { useThemed } from 'restyler';
 import 'tippy.js/animations/shift-away.css';
 import 'tippy.js/dist/tippy.css';
 
+let appendTo: Element | undefined;
+
 export interface TooltipProps extends TippyProps {}
 
 export const Tooltip = ({ content, ...rest }: TooltipProps) => {
   const ThemedTooltipContent = useThemed('div', 'tooltip.content');
+  const {
+    defaults: { standaloneTransitionOptions: { portal = undefined } = {} } = {}
+  } = useContext(SystemContext);
+  useIsomorphicLayoutEffect(() => {
+    if (appendTo || !portal) {
+      return;
+    }
+    portal.push(
+      <div
+        id="tooltips-container"
+        key="tooltips-container"
+        ref={v => v && (appendTo = v)}
+      />
+    );
+  }, [portal]);
   return (
     <Tippy
-      appendTo="parent"
+      appendTo={appendTo}
       animation="shift-away"
       content={<ThemedTooltipContent>{content}</ThemedTooltipContent>}
       {...rest}
