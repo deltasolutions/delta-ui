b62576cd33f1079e81a4ed05eb43fe2a33d5d71c
albnnc <albnnc@gmail.com>
2022-03-04 14:52:59 +0300

    fix(delta-data-table): types

--- packages/delta-data-table/lib/models/DataTableColumnOptions.tsx
+++ packages/delta-data-table/lib/models/DataTableColumnOptions.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export interface DataTableColumnOptions<T extends object> {
   key: string;
-  header?: string;
+  header?: ReactNode;
   width?: number;
   render?: (datum: T, index: number) => ReactNode;
 }
