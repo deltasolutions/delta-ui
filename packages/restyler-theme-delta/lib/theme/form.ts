77d16306f9ac293fb7df5db1d334209063cf96f9
albnnc <albnnc@gmail.com>
2022-03-05 16:04:54 +0300

    chore(restyler-theme-delta): 0.1.27

--- packages/restyler-theme-delta/lib/theme/form.ts
+++ packages/restyler-theme-delta/lib/theme/form.ts
@@ -1,26 +1,4 @@
-import { BasicTheme, mergeBasicThemes } from 'restyler';
-
-const actionButtonTheme = {
-  style: {
-    cursor: 'pointer',
-    backgroundColor: 'surface',
-    border: 'none',
-    svg: {
-      width: '26px',
-      height: '26px',
-      verticalAlign: 'middle',
-      color: 'inherit',
-      transition: 'color 0.2s linear'
-    },
-    '&:hover': {
-      svg: { color: 'primary' }
-    },
-    '&:disabled': {
-      pointerEvents: 'none',
-      opacity: 0.5
-    }
-  }
-};
+import { BasicTheme } from 'restyler';
 
 export const form: BasicTheme = {
   style: {
@@ -57,70 +35,6 @@ export const form: BasicTheme = {
             item: {
               style: { marginTop: 1, color: 'danger' }
             }
-          }
-        },
-        object: {
-          style: { display: 'flex', flexDirection: 'column' },
-          components: {
-            header: {
-              style: { weight: 500 }
-            },
-            body: {
-              style: {
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 3
-              }
-            }
-          }
-        },
-        array: {
-          style: {
-            display: 'flex',
-            flexDirection: 'column'
-          },
-          components: {
-            header: {
-              style: {
-                display: 'flex',
-                justifyContent: 'space-between',
-                weight: 500
-              },
-              components: {
-                actions: { style: { mr: 3 } }
-              }
-            },
-            body: {
-              style: {
-                display: 'flex',
-                flexDirection: 'column'
-              }
-            },
-            item: {
-              style: ({ hasPadding }) => ({
-                position: 'relative',
-                '& + &, &:first-of-type': {
-                  marginTop: 3,
-                  paddingTop: 3,
-                  borderTop: '1px solid',
-                  borderTopColor: 'border'
-                },
-                ...(hasPadding ? { pl: 3 } : {})
-              })
-            },
-            addButton: mergeBasicThemes({}, actionButtonTheme),
-            removeButton: mergeBasicThemes({}, actionButtonTheme, {
-              style: ({ theme: { space = [] } = {} }) => ({
-                position: 'absolute',
-                backdropFilter: 'blur(100px)',
-                top: '0',
-                right: space[3],
-                transform: 'translateY(-50%)'
-              })
-            }),
-            pagerButton: mergeBasicThemes({}, actionButtonTheme, {
-              marginRight: 2
-            })
           }
         }
       }
