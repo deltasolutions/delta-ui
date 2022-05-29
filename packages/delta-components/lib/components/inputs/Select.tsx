40894994dc587defdf7f30ca9341f559c94cc8f8
albnnc <albnnc@gmail.com>
2022-05-29 14:30:06 +0300

    wip: delta-components

--- packages/delta-components/lib/components/inputs/Select.tsx
+++ packages/delta-components/lib/components/inputs/Select.tsx
@@ -4,12 +4,13 @@ import {
   cloneElement,
   forwardRef,
   ReactElement,
+  useCallback,
   useMemo,
   useRef,
   useState,
 } from 'react';
 import { IoChevronDown } from 'react-icons/io5';
-import { useDrop, useUpdateEffect } from '../../hooks';
+import { DropRendererProps, useDrop, useUpdateEffect } from '../../hooks';
 import { FormWidgetProps } from '../../types';
 import { mergeRefs } from '../../utils';
 import { Button, ButtonProps } from '../Button';
@@ -50,37 +51,25 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
       return childrenArray.find(v => v.props.value === innerValue)?.props
         .children;
     }, [children, innerValue]);
-    useUpdateEffect(() => {
-      innerValue !== value && onChange?.(innerValue);
-    }, [innerValue]);
+    const handleChange = useCallback(
+      (nextValue: unknown) => {
+        nextValue !== innerValue && setInnerValue(nextValue);
+        nextValue !== value && onChange?.(nextValue);
+      },
+      [onChange]
+    );
     useUpdateEffect(() => {
       innerValue !== value && setInnerValue(value);
     }, [value]);
     const closeDropRef = useRef<undefined | (() => void)>();
     const [openDrop, anchorRef] = useDrop<HTMLDivElement>(
-      ({ handleClose }) => {
-        return (
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              overflow: 'hidden',
-              borderRadius: 3,
-            }}
-          >
-            {childrenArray.map(v =>
-              cloneElement(v, {
-                onClick: () => {
-                  setInnerValue(v.props.value);
-                  handleClose();
-                },
-              })
-            )}
-          </Box>
-        );
-      },
+      props => (
+        <SelectDrop handleChange={handleChange} {...props}>
+          {childrenArray}
+        </SelectDrop>
+      ),
       {
-        deps: [childrenArray],
+        deps: [childrenArray, handleChange],
         tailored: true,
         onClose: () => {
           closeDropRef.current = undefined;
@@ -131,6 +120,37 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
     );
   }
 );
+
+export interface SelectDropProps extends DropRendererProps {
+  children: ReactElement<SelectOptionProps>[];
+  handleChange: (v: unknown) => void;
+}
+
+export const SelectDrop = ({
+  children,
+  handleChange,
+  handleClose,
+}: SelectDropProps) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'hidden',
+        borderRadius: 3,
+      }}
+    >
+      {children.map(v =>
+        cloneElement(v, {
+          onClick: () => {
+            handleChange(v.props.value);
+            handleClose();
+          },
+        })
+      )}
+    </Box>
+  );
+};
 
 export interface SelectOptionProps
   extends Omit<ButtonProps, 'value' | 'children'> {
