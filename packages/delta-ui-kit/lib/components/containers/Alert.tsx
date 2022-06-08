5649f67106d7514cbbe863c78f73cc847fcf5565
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-06-08 23:24:07 +0300

    refactor(Alert): add animation

--- packages/delta-ui-kit/lib/components/containers/Alert.tsx
+++ packages/delta-ui-kit/lib/components/containers/Alert.tsx
@@ -10,14 +10,7 @@ export interface AlertProps extends BoxProps {
   color?: 'info' | 'warning' | 'success' | 'error';
   onClose?: () => void;
 }
-const slideIn = keyframes({
-  from: {
-    transform: 'scale(0.85)',
-  },
-  to: {
-    transform: 'scale(1)',
-  },
-});
+
 export const Alert = forwardRef<HTMLDivElement, AlertProps>(
   ({ color = 'primary', onClose, children, ...rest }, ref) => {
     return (
@@ -27,7 +20,7 @@ export const Alert = forwardRef<HTMLDivElement, AlertProps>(
           display: 'inline-flex',
           alignItems: 'flex-start',
           borderRadius: 4,
-          paddingX: '10px',
+          paddingX: 2,
           animation: `${slideIn} 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940)`,
           ...{
             info: {
@@ -50,20 +43,6 @@ export const Alert = forwardRef<HTMLDivElement, AlertProps>(
         }}
         {...rest}
       >
-        {/* <Button
-          sx={{
-            mt: 2,
-            mr: 1,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minHeight: 1,
-            minWidth: 1,
-          }}
-          tabIndex={-1}
-        >
-          <AiOutlineInfoCircle size={16} />
-        </Button> */}
         <span
           sx={{
             paddingY: 2,
@@ -98,68 +77,14 @@ export const Alert = forwardRef<HTMLDivElement, AlertProps>(
           <IoClose size={18} />
         </Button>
       </Box>
-      // <Box
-      //   ref={ref}
-      //   sx={{
-      //     alignItems: 'flex-start',
-      //     wordWrap: 'break-word',
-      //     display: 'flex',
-      //     width: '100%',
-      //     py: 1,
-      //     borderRadius: 4,
-      //     ...{
-      //       info: {
-      //         backgroundColor: 'info',
-      //         color: 'onInfo',
-      //       },
-      //       warning: {
-      //         backgroundColor: 'warning',
-      //         color: 'onWarning',
-      //       },
-      //       success: {
-      //         backgroundColor: 'success',
-      //         color: 'onSuccess',
-      //       },
-      //       error: {
-      //         backgroundColor: 'error',
-      //         color: 'onError',
-      //       },
-      //     }[color],
-      //   }}
-      //   {...rest}
-      // >
-      //   <AiOutlineInfoCircle
-      //     sx={{
-      //       flex: '0 0 auto',
-      //       width: '1.2rem',
-      //       height: '1.2rem',
-      //       verticalAlign: 'middle',
-      //       ml: '0.2rem',
-      //       mt: '0.1rem',
-      //       mr: '0.3rem',
-      //     }}
-      //   />
-      //   <Box sx={{ mr: '0.2rem' }}>{children}</Box>
-      //   <Button
-      //     sx={{
-      //       mr: '0.2em',
-      //       mt: '0.1em',
-      //       ml: 'auto',
-      //       borderRadius: '50%',
-      //       '&:hover': {
-      //         backgroundColor: {
-      //           info: 'accentInfo',
-      //           warning: 'accentWarning',
-      //           success: 'accentSuccess',
-      //           error: 'accentError',
-      //         }[color],
-      //       },
-      //     }}
-      //     onClick={onClose}
-      //   >
-      //     <IoClose size={18} sx={{ verticalAlign: 'middle' }} />
-      //   </Button>
-      // </Box>
     );
   }
 );
+const slideIn = keyframes({
+  from: {
+    transform: 'scale(0.85)',
+  },
+  to: {
+    transform: 'scale(1)',
+  },
+});
