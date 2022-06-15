25ea7cb670fcd20181ae9ccd08772d92ebf30d53
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-06-16 00:56:44 +0300

    feat(PairList): add row-space variant

--- packages/delta-ui-kit/lib/components/displays/PairList.stories.tsx
+++ packages/delta-ui-kit/lib/components/displays/PairList.stories.tsx
@@ -1,19 +1,27 @@
 import { Meta } from '@storybook/react';
 import { jsx } from '@theme-ui/core';
+import { compact } from '../../../docs/decorators';
+import { Switch } from '../inputs';
 import { PairList } from './PairList';
 
 export default {
   title: 'Displays/PairList',
+  decorators: [compact('700px')],
 } as Meta;
 
 const pairs = [
   ['Name', 'Delta BMC Scan'],
   ['Cron', '*/5 * * * *'],
-  ['Running', 'False'],
+  [
+    'Running',
+    'ddsadk jfsodijapso jfpasj fpdjsap ifjsadpoij fpoadsijfp ijadspfij paosijdfp japdsfijapsij fpoijapfi jpasijdf pijapojif d',
+  ],
   ['Next Execution', '15.04.2022'],
-  ['Last Status', 'SUCCESS'],
+  ['Last Status', <Switch />],
 ];
 
 export const Basics = () => <PairList pairs={pairs} />;
 
 export const Column = () => <PairList pairs={pairs} variant="column" />;
+
+export const RowSpace = () => <PairList pairs={pairs} variant="row-space" />;
