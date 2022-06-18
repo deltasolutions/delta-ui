769c8207e12d373ae8fef5627bdda3aa0d14b8a2
a.emelyanov <alexndr.emelyanov@gmail.com>
2022-06-18 18:34:17 +0300

    fix(Layot): hydration buug

--- packages/delta-ui-kit/lib/components/containers/Layout/LayoutMainHeader.tsx
+++ packages/delta-ui-kit/lib/components/containers/Layout/LayoutMainHeader.tsx
@@ -1,8 +1,16 @@
 import { useTheme } from '@emotion/react';
 import { jsx } from '@theme-ui/core';
 import { parseToRgb } from 'polished';
-import { forwardRef, useEffect, useMemo, useState } from 'react';
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { Theme } from '../../../defaults';
+import { mergeRefs } from '../../../utils';
 import { Box, BoxProps } from '../Box';
 
 export const layoutHeaderHeight = 52;
@@ -14,25 +22,23 @@ export const LayoutMainHeader = forwardRef<
   LayoutMainHeaderProps
 >((props: LayoutMainHeaderProps, propsRef) => {
   const theme = useTheme() as Theme;
-  const [offset, setOffset] = useState(
-    typeof window === 'undefined' ? 0 : window.scrollY
-  );
-  const style = useMemo(() => {
-    const color = parseToRgb(theme.colors.exterior);
-    const ratio = Math.min(offset / layoutHeaderHeight, 1);
-    const backgroundColor = `rgba(${color.red}, ${color.green}, ${color.blue}, ${ratio})`;
-    return { backgroundColor };
-  }, [offset]);
+  const ref = useRef<HTMLDivElement>(null);
+  const mergedRef = useMemo(() => mergeRefs([ref, propsRef]), []);
+  const color = parseToRgb(theme.colors.exterior);
+  const handleScroll = useCallback(() => {
+    const ratio = Math.min(window.scrollY / layoutHeaderHeight, 1);
+    ref.current?.setAttribute(
+      'style',
+      `background-color: rgba(${color.red}, ${color.green}, ${color.blue}, ${ratio});`
+    );
+  }, [color]);
   useEffect(() => {
-    const handleScroll = () => setOffset(window.scrollY);
-    handleScroll();
     addEventListener('scroll', handleScroll);
     return () => removeEventListener('scroll', handleScroll);
   }, []);
   return (
     <Box
-      ref={propsRef}
-      style={style}
+      ref={mergedRef}
       sx={{
         position: 'sticky',
         top: 0,
