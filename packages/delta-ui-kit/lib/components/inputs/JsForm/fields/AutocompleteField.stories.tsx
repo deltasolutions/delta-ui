5ecd1c3049fddb8802685f4ad62f8edfc4420086
albnnc <albnnc@gmail.com>
2022-07-05 17:53:58 +0300

    feat(delta-ui-kit): better autocomplete initials

--- packages/delta-ui-kit/lib/components/inputs/JsForm/fields/AutocompleteField.stories.tsx
+++ packages/delta-ui-kit/lib/components/inputs/JsForm/fields/AutocompleteField.stories.tsx
@@ -1,84 +1,99 @@
-import { jsx } from '@theme-ui/core';
-import { createFormStory, formStoryMeta } from '../../../../../docs/utils';
-import { Box } from '../../../containers';
+import { jsx } from "@theme-ui/core";
+import { createFormStory, formStoryMeta } from "../../../../../docs/utils";
+import { Box } from "../../../containers";
+import { AutocompleteOptionsInit } from "./AutocompleteField";
 
 export default {
   ...formStoryMeta,
-  title: 'JsForm/Autocomplete',
+  title: "JsForm/Autocomplete",
 };
 
 const oneOf = [
   {
-    title: 'A',
-    const: 'aaa',
+    title: "A",
+    const: "aaa",
   },
   {
-    title: 'B',
-    const: 'bbb',
+    title: "B",
+    const: "bbb",
   },
   {
-    title: 'C',
-    const: 'ccc',
+    title: "C",
+    const: "ccc",
   },
 ];
 
 export const Basics = createFormStory({
   schema: {
-    type: 'object',
+    type: "object",
     properties: {
       single: {
-        type: 'string',
-        title: 'Autocomplete (single)',
+        type: "string",
+        title: "Autocomplete (single)",
         oneOf,
-        layout: { field: 'autocomplete' },
+        layout: { field: "autocomplete" },
       },
       multiple: {
-        type: 'array',
-        title: 'Autocomplete (multiple)',
+        type: "array",
+        title: "Autocomplete (multiple)",
         items: { oneOf },
-        layout: { field: 'autocomplete' },
+        layout: { field: "autocomplete" },
       },
     },
   },
   initialValue: {},
 });
 
+export const InitialValue = createFormStory({
+  schema: {
+    type: "string",
+    title: "With default value",
+    oneOf,
+    layout: { field: "autocomplete" },
+  },
+  initialValue: "aaa",
+});
+
+export const InitialValueWithoutOption = createFormStory({
+  schema: {
+    type: "string",
+    title: "With default value",
+    oneOf: [],
+    layout: { field: "autocomplete" },
+  },
+  initialValue: "aaa",
+});
+
 export const CustomOptions = createFormStory({
   schema: {
-    type: 'array',
-    title: 'With Custom Options',
+    type: "array",
+    title: "With custom options",
     layout: {
-      field: 'autocomplete',
-      source: {
-        entity: 'DATACENTER',
-        filter: 'id=ne=1234',
-      },
+      field: "autocomplete",
+      target: "ANY_VALUE",
     },
   },
-  initialValue: [],
+  initialValue: ["aaa"],
   registry: {
     utils: {
-      getAutocompleteOptions: (query: string, _source: unknown) => {
-        return new Array(5).fill(undefined).map(() => {
-          const x =
-            (query ? query + '-' : '') + Math.random().toString().slice(-8);
-          return {
-            title: x,
-            const: x,
-            render: () => (
-              <Box
-                sx={{
-                  p: 1,
-                  borderRadius: 2,
-                  backgroundColor: 'info',
-                  color: 'onInfo',
-                }}
-              >
-                {x}
-              </Box>
-            ),
-          };
-        });
+      getAutocompleteOptions: (
+        { target: _, query, value }: AutocompleteOptionsInit,
+      ) => {
+        if (Array.isArray(value)) {
+          return [{
+            title: `Initial title for "${value[0]}"`,
+            const: value[0],
+          }];
+        }
+        return fetch(`https://jsonplaceholder.typicode.com/users?q=${query}`)
+          .then((v) => v.json())
+          .then((v) =>
+            v.map((t) => ({
+              title: t.name,
+              const: t.id,
+            }))
+          )
+          .catch(() => []);
       },
     },
   },
--- packages/delta-ui-kit/lib/components/inputs/JsForm/templates/ErrorList.tsx
+++ packages/delta-ui-kit/lib/components/inputs/JsForm/templates/ErrorList.tsx
@@ -1,6 +1,6 @@
-import { jsx } from '@theme-ui/core';
-import { Validity } from 'delta-jsf';
-import { Box } from '../../../containers';
+import { jsx } from "@theme-ui/core";
+import { Validity } from "delta-jsf";
+import { Box } from "../../../containers";
 
 export interface ErrorListProps {
   validity?: Validity;
@@ -15,16 +15,14 @@ export const ErrorList = ({
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         gap: 2,
-        color: 'error',
+        color: "error",
         fontWeight: 600,
       }}
     >
-      {errors.map(e => (
-        <Box key={e}>{e}</Box>
-      ))}
+      {Array.from(new Set(errors)).map((e) => <Box key={e}>{e}</Box>)}
     </Box>
   );
 };
