8e2511fac9f11d8ba839ed35820b53fe0af571c2
albnnc <albnnc@gmail.com>
2022-07-11 16:05:04 +0300

    feat(delta-ui-kit): unitize "wide" container variants

--- packages/delta-ui-kit/lib/components/containers/Modal/ModalBody.tsx
+++ packages/delta-ui-kit/lib/components/containers/Modal/ModalBody.tsx
@@ -2,18 +2,20 @@ import { jsx } from '@theme-ui/core';
 import { forwardRef } from 'react';
 import { Box, BoxProps } from '../Box';
 
-export interface ModalBodyProps extends BoxProps {}
+export interface ModalBodyProps extends BoxProps {
+  variant?: 'wide';
+}
 
 export const ModalBody = forwardRef<HTMLDivElement, ModalBodyProps>(
-  (props, ref) => {
+  ({ variant, ...rest }, ref) => {
     return (
       <Box
         ref={ref}
         sx={{
-          paddingX: 5,
+          paddingX: variant === 'wide' ? 0 : 5,
           paddingY: 4,
         }}
-        {...props}
+        {...rest}
       />
     );
   }
